925e408aac5dd00fb90d59723c57e3b7679a1a1e
343dev <343dev@users.noreply.github.com>
2018-12-09 21:51:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-function delugeRpc() {
-  console.log('hello');
+const DelugeRPC = require('./lib/deluge-rpc');
+
+async function delugeWebCli({ args = [], host, password = 'deluge' }) {
+  if (!host) {
+    throw new Error('Please provide deluge-web server URL.');
+  }
+
+  const deluge = new DelugeRPC(host, password);
+  const isConnected = await deluge.call('web.connected');
+
+  if (isConnected) {
+    await deluge.connect();
+  }
+
+  if (args.length) {
+    console.log(args);
+  }
 }
 
-module.exports = delugeRpc;
+module.exports = delugeWebCli;
