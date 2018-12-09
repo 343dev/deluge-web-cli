35f3651b4994066cc4207b5f836af695f2290144
343dev <343dev@users.noreply.github.com>
2018-12-09 21:49:16 +0500

    Add `host` and `password` options to CLI

--- cli.js
+++ cli.js
@@ -4,9 +4,17 @@ const program = require('commander');
 const delugeWebCli = require('.');
 
 program
+  .option('--host [url]', 'deluge-web server URL')
+  .option('--password [pwd]', 'deluge-web server password');
+
+program
+  .usage('[options] <file ...>')
   .version(require('./package').version, '-v, --version')
   .description(require('./package').description)
   .parse(process.argv);
 
 delugeWebCli({
+  args: program.args,
+  host: program.host,
+  password: program.password,
 });
